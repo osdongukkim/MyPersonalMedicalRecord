ca483bc79bbaa77b5da7c65390883792d62bfb2d
osdongukkim <66855106+osdongukkim@users.noreply.github.com>
2020-06-14 18:01:52 +0900

    Add files via upload

---
+++ Mymedicalrecord1.py
@@ -0,0 +1,17 @@
+#!C:\Users\김동욱\AppData\Local\Programs\Python\Python38-32\python.exe
+print("content-type:text/html; charset=UTF-8\n")
+
+print('''<!doctype html>
+<html lang="ko">
+  <head>
+    <meta charset="utf-8">
+    <title>연습</title>
+  </head>
+    <body>
+      <script>
+        input type="button" name="login" value="Login" onclick="alert('changed')"
+        input type="text" name="login" value="Login" onchange="alert('changed')"
+      </script>
+    </body>
+  </html>''')
+  
